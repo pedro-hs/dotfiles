9b5ab897edd712945285c8ca6360107eec5a97f7
pedrosantosarruda <pedro.arruda@pisomtech.com.br>
2021-03-20 22:02:13 -0300

    remove messages

--- install.sh
+++ install.sh
@@ -1,85 +1,27 @@
-set -e
 # INSTALL
-message="updating/n"
 apt update -y
 apt upgrade -y
-clear
-echo -en "$message"
-
-message+="installing libre office calc/n"
-apt install -y libreoffice-calc
-clear
-echo -en "$message"
-
-message+="installing libre office writer/n"
-apt install -y libreoffice-writer
-clear
-echo -en "$message"
-
-message+="installing gnome-tweaks/n"
-apt install -y gnome-tweaks
-clear
-echo -en "$message"
-
-message+="installing terminator/n"
-apt install -y terminator
-clear
-echo -en "$message"
-
-message+="installing ranger/n"
-apt install -y ranger
-clear
-echo -en "$message"
-
-message+="installing vlc/n"
-snap install vlc
-clear
-echo -en "$message"
-
-message+="installing sound indicator/n"
+apt install -y libreoffice-calc libreoffice-writer gnome-tweaks terminator ranger
 snap install vlc indicator-sound-switcher
-clear
-echo -en "$message"
-
-message+="installing vscode/n"
 snap install --classic code
-clear
-echo -en "$message"
 
 # CONFIGURE PROGRAMS
-message+="setting git"
 rm ~/.gitconfig
 cp .gitconfig ~/
-clear
-echo -en "$message"
-
-message+="setting terminator"
 rm -rf ~/.config/terminator
 mkdir ~/.config/terminator
 cp config ~/.config/terminator
-clear
-echo -en "$message"
-
-message+="updating bashrc"
 cat .bashrc | sed -e 1d >> ~/.bashrc
-clear
-echo -en "$message"
-
 
 # CONFIGURE VIM
-message="setting vim"
 installation_dir=$(pwd)
 cd ~/.config
 git clone https://github.com/pedro-hs/nvim.git
 cd nvim
 sh install.sh
 cd $installation_dir
-clear
-echo -en "$message"
-
 
 # CONFIGURE SO
-message="setting so preferences"
 ubuntu-drivers autoinstall
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-seconds true
@@ -91,11 +33,8 @@ gsettings set org.gnome.desktop.interface gtk-key-theme='Emacs'
 gsettings set org.gnome.shell.extensions.desktop-icons show-trash false
 gsettings set org.gnome.shell.extensions.desktop-icons show-home false
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 30
-clear
-echo -en "$message"
 
 # FIX
-message="fixing keyboard"
 apt install gcc make
 wget https://github.com/jergusg/k380-function-keys-conf/archive/refs/tags/v1.1.zip
 # Connect keyboard
@@ -106,8 +45,6 @@ sudo k380_conf -d /dev/hidrawX -f on
 cd ..
 rm -rf k380-function-keys-conf-1.1
 rm k380-function-keys-conf-1.1.zip
-clear
-echo -en "$message"
 
 # TODO: pair bluetooth keyboard on startup
 # TODO: configure ranger? vimfm? cfiles?
