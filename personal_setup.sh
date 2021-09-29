696250973c319938a7ad4836480a7180e3cb80e6
Pedro Santos Arruda <42540954+pedro-hs@users.noreply.github.com>
2021-09-29 01:31:11 -0300

    Update personal_setup.sh

--- personal_setup.sh
+++ personal_setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 source show_status.sh
 
-print_message "INSTALL TRANSMISSION"
-sudo apt install -y transmission
+print_message "INSTALL TRANSMISSION, VLC"
+sudo apt install -y transmission vlc
 
 print_message "INSTALL STEAM"
 wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb && sudo apt install -y ./steam.deb
