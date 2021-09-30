de591e7b3e189959877f8fb1272db597c19dacd1
Pedro Santos Arruda <42540954+pedro-hs@users.noreply.github.com>
2021-09-29 22:34:51 -0300

    Update and rename clone_dotfiles.sh to run_install.sh

--- run_install.sh
+++ run_install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-initial_location="$(pwd)"
 mkdir /home/$(whoami)/src
 cd /home/$(whoami)/src
 git clone git@github.com:pedro-hs/dotfiles.git
 cd dotfiles
-rm -rf "$initial_location/dotfiles"
+source setup_zorin.sh
